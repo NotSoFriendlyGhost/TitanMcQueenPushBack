fc125f6f94d9d6bddeb2613128113d231e019ce6
Caspar Chen <casparagusc@gmail.com>
2025-11-27 09:33:40 -0600

    removed active brake

--- src/main.cpp
+++ src/main.cpp
@@ -55,7 +55,7 @@ void initialize() {
 
   // Configure your chassis controls
   chassis.opcontrol_curve_buttons_toggle(true);  // Enables modifying the controller curve with buttons on the joysticks
-  chassis.opcontrol_drive_activebrake_set(2.0);  // Sets the active brake kP. We recommend ~2.  0 will disable.
+  chassis.opcontrol_drive_activebrake_set(0.0);  // Sets the active brake kP. We recommend ~2.  0 will disable.
   // chassis.opcontrol_curve_default_set(0.0, 0.0);  // Defaults for curve. If using tank, only the first parameter is used. (Comment this line out if you have an SD card!)
 
   // Set the drive to your own constants from autons.cpp!
@@ -86,8 +86,8 @@ void initialize() {
   // Initialize chassis and auton selector
   chassis.initialize();
 
-  logo.focus();
-  // ez::as::initialize();
+  // logo.focus();
+  ez::as::initialize();
   master.rumble(chassis.drive_imu_calibrated() ? "." : "---");
 }
 
