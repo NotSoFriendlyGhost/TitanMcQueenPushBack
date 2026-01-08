921bda20bbbcee24c7128a20c208dd584b3c39be
Caspar Chen <casparagusc@gmail.com>
2026-01-08 08:53:38 -0600

    Changed motor ports

--- include/subsystems.hpp
+++ include/subsystems.hpp
@@ -11,8 +11,8 @@ extern Drive chassis;
 // inline pros::adi::DigitalIn limit_switch('A');
 
 // Intake motors
-inline pros::Motor intake(-15);
-inline pros::Motor topMotor(16);
+inline pros::Motor intake(4);
+inline pros::Motor topMotor(-7);
 
 // Pneumatics
 inline ez::Piston tongue('B');
