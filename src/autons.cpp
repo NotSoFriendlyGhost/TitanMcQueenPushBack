ac51f45b6e4e298466e2b72642760e1d7f74c023
Caspar Chen <casparagusc@gmail.com>
2026-03-10 22:52:29 -0500

    autons working

--- src/autons.cpp
+++ src/autons.cpp
@@ -659,8 +659,9 @@ void fast_wing_auton(){
   slopeScore();
   pros::delay(1600);
   intake_in();
+  tongue.set(0);
 
-  chassis.pid_odom_set({{-45.546_in, 58.965_in}, fwd, DRIVE_SPEED}, true);
+  chassis.pid_odom_set({{-39.546_in, 58.965_in}, fwd, DRIVE_SPEED}, true);
   chassis.pid_wait();
 
   chassis.pid_odom_set({{{-30.546_in, 58.465_in}, rev, BACK_DRIVE_SPEED},
@@ -672,23 +673,23 @@ void fast_wing_auton(){
 void right_fast_wing_auton(){
   set_position(-46.64, -13.833, 90);
   intake_in();
-  chassis.pid_odom_set({{{-23.086_in, -19.523_in}, fwd, DRIVE_SPEED},
+  chassis.pid_odom_set({{{-21.086_in, -19.523_in}, fwd, DRIVE_SPEED},
                         {{-45.64_in, -47.965_in}, fwd, DRIVE_SPEED},
                        },
                        true);
   chassis.pid_wait_until_index_started(0);
-  pros::delay(100);
+  pros::delay(20);
   tongue.set(1);
   chassis.pid_wait();
   chassis.pid_turn_set({-60.194_in, -47.965_in}, fwd, DRIVE_SPEED);
   chassis.pid_wait();
 
-  chassis.pid_odom_set({{-25.546, -47.965}, rev, DRIVE_SPEED}, true);
+  chassis.pid_odom_set({{-26.546, -47.965}, rev, DRIVE_SPEED}, true);
   // tongue.set(0);
   // chassis.pid_wait_until(22);
   chassis.pid_wait();
   slopeScore();
-  pros::delay(1100);
+  pros::delay(1400);
   intake_in();
 
   chassis.pid_odom_set({{-45.546_in, -37.965_in}, fwd, DRIVE_SPEED}, true);
@@ -741,21 +742,21 @@ void wing_auton(){
 void right_wing_auton(){
   set_position(-46.64, -13.833, 90);
   intake_in();
-  chassis.pid_odom_set({{{-23.086_in, -19.523_in}, fwd, DRIVE_SPEED},
+  chassis.pid_odom_set({{{-21.086_in, -19.523_in}, fwd, DRIVE_SPEED},
                         {{-45.64_in, -47.965_in}, fwd, DRIVE_SPEED},
                        },
                        true);
   chassis.pid_wait_until_index_started(0);
-  pros::delay(50);
+  pros::delay(20);
   tongue.set(1);
   chassis.pid_wait();
-  chassis.pid_turn_set({-56.194_in, -48.965_in}, fwd, DRIVE_SPEED);
+  chassis.pid_turn_set({-56.194_in, -47.965_in}, fwd, DRIVE_SPEED);
   chassis.pid_wait();
-  chassis.pid_odom_set({{-56.194_in, -48.965_in}, fwd, DRIVE_SPEED}, true);
+  chassis.pid_odom_set({{-56.194_in, -47.965_in}, fwd, DRIVE_SPEED}, true);
   chassis.pid_wait();
   pros::delay(500);
 
-  chassis.pid_odom_set({{-26.546, -47.965}, rev, DRIVE_SPEED}, true);
+  chassis.pid_odom_set({{-27.546, -47.965}, rev, DRIVE_SPEED}, true);
   pros::delay(100);
   tongue.set(0);
   chassis.pid_wait();
